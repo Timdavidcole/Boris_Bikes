ddcc22eca5d0f718985f6ab138c53892c0d1fab6
jamie palmer <james.palmer@cantab.net>
2019-06-26 16:05:02 +0100

    finished stage 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,26 @@
 DockingStation = Class.new
 
 class DockingStation
-
+  def initialize
+    @docked_bikes = []
+  end
+  attr_reader :docked_bikes
+  # attr_reader currently does the same thing as check_bikes
   def release_bike
-    Bike.new
+    if @docked_bikes == []
+      fail "no bikes docked"
+    else
+      Bike.new
+    end
+  end
+
+  def dock_bike (bike)
+    @docked_bikes.push(bike)
+
+  end
+
+  def check_bikes
+    @docked_bikes
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,12 @@ describe DockingStation do
 end
 
 describe DockingStation do
+  it 'docks bike, when dock_bike method is called' do
+    expect(subject).to respond_to(:dock_bike).with(1).argument
+  end
+end
+
+describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
@@ -15,13 +21,41 @@ end
 describe DockingStation do
   it 'release_bike initializes Bike object' do
     docking_station = DockingStation.new
+    docking_station.dock_bike(Bike.new)
     expect(docking_station.release_bike.is_a? Bike).to eq true
+  end
+end
+describe DockingStation do
+  it 'shows docked bikes when check_bikes is called ' do
+    docking_station = DockingStation.new
+    bike1 = Bike.new
+    docking_station.dock_bike(bike1)
+    expect(docking_station.check_bikes).to eq [bike1]
   end
 end
 
 describe DockingStation do
+  it 'dock the bike when dock_bike method is called ' do
+    docking_station = DockingStation.new
+    bike1 = Bike.new
+
+    expect(docking_station.dock_bike(bike1)).to eq [bike1]
+  end
+end
+
+
+
+describe DockingStation do
   it 'release_bike initializes Bike object that is working' do
     docking_station = DockingStation.new
+    docking_station.dock_bike(Bike.new)
     expect(docking_station.release_bike.working?).to eq true
+  end
+end
+
+describe DockingStation do
+  it 'raises an error when release_bike called when no bikes docked' do
+    docking_station = DockingStation.new
+    expect { docking_station.release_bike }.to raise_error("no bikes docked")
   end
 end
